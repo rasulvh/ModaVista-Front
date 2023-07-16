a5414c5b9561a10eb032fcf7a121ba2b9bd07158
Rasul Hasanov <rasulhasanov357@gmail.com>
2023-07-16 21:37:47 +0400

    home slider was done

--- assets/js/home.js
+++ assets/js/home.js
@@ -19,4 +19,37 @@ $(document).ready(function () {
         $(".menu").toggleClass("menu-open");
     })
 
+    //Slider
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 5000,
+    }).on('changed.owl.carousel', function (event) {
+        // Remove the previous animation classes
+        $('.item .text span').removeClass('animate__animated animate__fadeInUp');
+        $('.item .text h1').removeClass('animate__animated animate__fadeInUp');
+        $('.item .text p').removeClass('animate__animated animate__fadeInUp');
+        $('.item .text a').removeClass('animate__animated animate__fadeInUp');
+        $('.item .sale').removeClass('animate__animated animate__rotateInDownRight');
+
+        // Select the current slide's text element and add the animation classes
+        var currentSlide = event.item.index;
+        $('.item .text').eq(currentSlide).find('h1').addClass('animate__animated animate__fadeInUp');
+        $('.item .text').eq(currentSlide).find('p').addClass('animate__animated animate__fadeInUp');
+        $('.item .text').eq(currentSlide).find('span').addClass('animate__animated animate__fadeInUp');
+        $('.item .text').eq(currentSlide).find('a').addClass('animate__animated animate__fadeInUp');
+        $('.item').eq(currentSlide).find('.sale').addClass('animate__animated animate__rotateInDownRight');
+    });
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+    $('#slider .fa-chevron-right').click(function () {
+        owl.trigger('next.owl.carousel');
+    })
+    $('#slider .fa-chevron-left').click(function () {
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+
 })
