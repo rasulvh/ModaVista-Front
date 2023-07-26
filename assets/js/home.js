626afb02052bb71d349256c8d423dcac53f2cd8e
Rasul Hasanov <rasulhasanov357@gmail.com>
2023-07-26 23:23:56 +0400

    home instagram part was ready

--- assets/js/home.js
+++ assets/js/home.js
@@ -96,10 +96,10 @@ $(document).ready(function () {
         });
     }
 
-    $('.datas .fa-chevron-right').click(function () {
+    $('#women-banner .datas .fa-chevron-right').click(function () {
         womenSlider1.trigger('next.owl.carousel');
     })
-    $('.datas .fa-chevron-left').click(function () {
+    $('#women-banner .datas .fa-chevron-left').click(function () {
         womenSlider1.trigger('prev.owl.carousel', [300]);
     })
 
@@ -122,10 +122,10 @@ $(document).ready(function () {
         });
     }
 
-    $('.datas .fa-chevron-right').click(function () {
+    $('#women-banner .datas .fa-chevron-right').click(function () {
         womenSlider2.trigger('next.owl.carousel');
     })
-    $('.datas .fa-chevron-left').click(function () {
+    $('#women-banner .datas .fa-chevron-left').click(function () {
         womenSlider2.trigger('prev.owl.carousel', [300]);
     })
 
@@ -148,10 +148,10 @@ $(document).ready(function () {
         });
     }
 
-    $('.datas .fa-chevron-right').click(function () {
+    $('#women-banner .datas .fa-chevron-right').click(function () {
         womenSlider3.trigger('next.owl.carousel');
     })
-    $('.datas .fa-chevron-left').click(function () {
+    $('#women-banner .datas .fa-chevron-left').click(function () {
         womenSlider3.trigger('prev.owl.carousel', [300]);
     })
 
@@ -174,11 +174,140 @@ $(document).ready(function () {
         });
     }
 
-    $('.datas .fa-chevron-right').click(function () {
+    $('#women-banner .datas .fa-chevron-right').click(function () {
         womenSlider4.trigger('next.owl.carousel');
     })
-    $('.datas .fa-chevron-left').click(function () {
+    $('#women-banner .datas .fa-chevron-left').click(function () {
         womenSlider4.trigger('prev.owl.carousel', [300]);
+    })
+
+    //Men Clothing Tab Menu
+    $('#men-banner .head p').click(function () {
+        $('#men-banner p.active').addClass('de-active')
+        $('#men-banner p.active').removeClass('active')
+        if ($(this).hasClass('de-active')) {
+            $(this).removeClass('de-active')
+        }
+        $(this).addClass('active')
+
+        let dataId = $(this).attr('data-id')
+
+        $('.datas div.owl-carousel').each(function () {
+            if ($(this).attr('data-id') == dataId) {
+                if ($(this).hasClass('d-none')) {
+                    $(this).removeClass('d-none')
+                }
+            }
+            else {
+                if (!$(this).hasClass('d-none')) {
+                    $(this).addClass('d-none')
+                }
+            }
+        })
+    })
+
+    var menSlider1 = $('#men-carousel-1');
+
+    if (menSlider1.children().length > 3) {
+        menSlider1.owlCarousel({
+            loop: true,
+            margin: 10,
+            items: 3,
+            autoplay: true,
+            autoplayTimeout: 3000,
+        });
+    }
+    else {
+        menSlider1.owlCarousel({
+            loop: false,
+            margin: 10,
+            items: 3,
+        });
+    }
+
+    $('#men-banner .datas .fa-chevron-right').click(function () {
+        menSlider1.trigger('next.owl.carousel');
+    })
+    $('#men-banner .datas .fa-chevron-left').click(function () {
+        menSlider1.trigger('prev.owl.carousel', [300]);
+    })
+
+    var menSlider2 = $('#men-carousel-2');
+
+    if (menSlider2.children().length > 3) {
+        menSlider2.owlCarousel({
+            loop: true,
+            margin: 10,
+            items: 3,
+            autoplay: true,
+            autoplayTimeout: 3000,
+        });
+    }
+    else {
+        menSlider2.owlCarousel({
+            loop: false,
+            margin: 10,
+            items: 3,
+        });
+    }
+
+    $('#men-banner .datas .fa-chevron-right').click(function () {
+        menSlider2.trigger('next.owl.carousel');
+    })
+    $('#men-banner .datas .fa-chevron-left').click(function () {
+        menSlider2.trigger('prev.owl.carousel', [300]);
+    })
+
+    var menSlider3 = $('#men-carousel-3');
+
+    if (menSlider3.children().length > 3) {
+        menSlider3.owlCarousel({
+            loop: true,
+            margin: 10,
+            items: 3,
+            autoplay: true,
+            autoplayTimeout: 3000,
+        });
+    }
+    else {
+        menSlider3.owlCarousel({
+            loop: false,
+            margin: 10,
+            items: 3,
+        });
+    }
+
+    $('#men-banner .datas .fa-chevron-right').click(function () {
+        menSlider3.trigger('next.owl.carousel');
+    })
+    $('#men-banner .datas .fa-chevron-left').click(function () {
+        menSlider3.trigger('prev.owl.carousel', [300]);
+    })
+
+    var menSlider4 = $('#men-carousel-4');
+
+    if (menSlider4.children().length > 3) {
+        menSlider4.owlCarousel({
+            loop: true,
+            margin: 10,
+            items: 3,
+            autoplay: true,
+            autoplayTimeout: 3000,
+        });
+    }
+    else {
+        menSlider4.owlCarousel({
+            loop: false,
+            margin: 10,
+            items: 3,
+        });
+    }
+
+    $('#men-banner .datas .fa-chevron-right').click(function () {
+        menSlider4.trigger('next.owl.carousel');
+    })
+    $('#men-banner .datas .fa-chevron-left').click(function () {
+        menSlider4.trigger('prev.owl.carousel', [300]);
     })
 
 })
